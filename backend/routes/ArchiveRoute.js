67ca44022ceea2b514c403633fb62f062734724b
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2024-09-08 11:49:34 -0700

    teste code chat

--- backend/routes/ArchiveRoute.js
+++ backend/routes/ArchiveRoute.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const { addFolderValidator, updateFolderValidator } = require("../helpers/ValidatorFolder");
 const { addArchiveValidator } = require("../helpers/ValidatorArchive");
-const auth = require("../middlewares/AutheMiddleware");
+// const auth = require("../middlewares/AutheMiddleware");
+const verifyToken = require("../middlewares/verifyToken");
 
 const { addArchive, getArchive, editArchiveById, updateArchiveById, deleteArhiveById } = require("../controllers/ArchiveController");
 
-router.post("/add_archive", auth, addFolderValidator, addArchive);
+// router.post("/add_archive", auth, addFolderValidator, addArchive);
+// Route protégée nécessitant une authentification
+router.post("/add_archive", verifyToken, addFolderValidator, addArchive);
 router.get("/get_archive", getArchive);
 router.get("/edit_archive/:id", addArchiveValidator, editArchiveById);
 router.put("/update_archive/:id", updateFolderValidator, updateArchiveById); // Nouvelle route pour mettre à jour un dossier
