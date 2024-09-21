e843186280455a911f03d1e8019416c688a205ce
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2024-09-21 11:21:38 -0700

    mis a page de table courrier

--- frontend/src/pages/visa/Visa.jsx
+++ frontend/src/pages/visa/Visa.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import "./visa.scss"
+import customerList from '../../Data/user.json'
+import TablePersonalize from '../../components/table/TablePersonalize'
+
+
+const customerTableHead = [
+  '',
+  'name',
+  'email',
+  'phone',
+  'total orders',
+  'total spend',
+  'location'
+]
+
+const renderHead = (item, index) => <th key={index}>{item}</th>
+
+const renderBody = (item, index) => (
+  <tr className='tr' key={index}>
+      <td className='td'>{item.id}</td>
+      <td className='td'>{item.name}</td>
+      <td className='td'>{item.email}</td>
+      <td className='td'>{item.phone}</td>
+      <td className='td'>{item.total_orders}</td>
+      <td className='td'>{item.total_spend}</td>
+      <td className='td'>{item.location}</td>
+  </tr>
+)
+
+
 
 const Visa = () => {
   return (
-    <div className='row'>
-      Visa
+    <div className='rowx'>
+      <TablePersonalize
+        limit='10'
+        headData={customerTableHead}
+        renderHead={(item, index) => renderHead(item, index)}
+        bodyData={customerList}
+        renderBody={(item, index) => renderBody(item, index)}
+       />
     </div>
   )
 }
